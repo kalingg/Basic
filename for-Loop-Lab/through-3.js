ef440c1d204b829cf24e6d01caf4025b0eae8bc5
Kalin Georgiev <100343638+kalingg@users.noreply.github.com>
2023-05-17 23:09:07 +0300

    Add files via upload

---
+++ for-Loop-Lab/through-3.js
@@ -0,0 +1,11 @@
+function max (input){
+
+    let num = Number (input);
+
+    for(let i = 1; i<=num; i+=3){
+        console.log(i);
+    }
+}
+max(["10"]);
+max(["7"]);
+max(["15"]);
---
+++ for-Loop-Lab/toFrom.js
@@ -0,0 +1,12 @@
+function from (input){
+
+    let from = Number (input);
+
+    for(let i = from; i>=1; i--){
+        console.log(i);
+    };
+
+};
+from(["2"]);
+from(["3"]);
+from(["5"]);
