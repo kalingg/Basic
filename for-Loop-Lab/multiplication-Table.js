5fd5774ca22f8e6ea6cc74c2d35126e717152dec
Kalin Georgiev <100343638+kalingg@users.noreply.github.com>
2023-05-19 22:30:21 +0300

    Add files via upload

---
+++ for-Loop-Lab/multiplication-Table.js
@@ -0,0 +1,12 @@
+function multipli (input){
+
+    let num = Number (input[0]);
+
+    for (let i=1; i<= 10; i++){
+
+        console.log(`${i} * ${num} = ${i * num}`)
+    }
+
+
+}
+multipli (["5"])
